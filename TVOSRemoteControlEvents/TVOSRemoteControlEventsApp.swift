12df70800e1ecc13f2bf5c82113045a37da97b6b
mkotyak <kotyak.maria@gmail.com>
2024-06-05 11:18:41 +0300

    Initial commit

---
+++ TVOSRemoteControlEvents/TVOSRemoteControlEventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVOSRemoteControlEventsApp.swift
+//  TVOSRemoteControlEvents
+//
+//  Created by Maria Kotyak on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TVOSRemoteControlEventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
