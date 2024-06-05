75f8b48286f9bdccc342b935f5a4b8b877f35b9f
mkotyak <kotyak.maria@gmail.com>
2024-06-05 12:02:01 +0300

    Experimented with .onExitCommand

--- TVOSRemoteControlEvents/ContentView.swift
+++ TVOSRemoteControlEvents/ContentView.swift
@@ -4,31 +4,52 @@ struct ContentView: View {
     @State private var text: String = "Hello, world!"
 
     var body: some View {
-        Group {
-            Color.yellow.opacity(0.5).ignoresSafeArea()
-                .overlay {
-                    Text(text)
-                        .font(.largeTitle)
-                        .bold()
-                        .foregroundStyle(.black)
+        NavigationStack {
+            Group {
+                Color.yellow.ignoresSafeArea()
+                    .overlay {
+                        ZStack {
+                            Text(text)
+                                .font(.largeTitle)
+                                .bold()
+                                .foregroundStyle(.black)
+
+                            VStack {
+                                Spacer()
+
+                                NavigationLink("Tap me") {
+                                    Color.blue.ignoresSafeArea()
+                                        .overlay {
+                                            Text("I am a sheet")
+                                                .font(.largeTitle)
+                                                .bold()
+                                                .foregroundStyle(.white)
+                                                .focusable()
+                                                .onExitCommand {
+                                                    debugPrint("Sheet is exit")
+                                                }
+                                        }
+                                }
+                            }
+                        }
+                    }
+            }
+            .onExitCommand {
+                text = "Exit"
+            }
+            .onMoveCommand { moveCommandDirection in
+                switch moveCommandDirection {
+                case .down:
+                    text = "Tap down"
+                case .up:
+                    text = "Tap up"
+                case .left:
+                    text = "Tap left"
+                case .right:
+                    text = "Tap right"
+                @unknown default:
+                    assertionFailure("Unknown Command Direction")
                 }
-        }
-        .focusable()
-        .onExitCommand {
-            text = "Exit"
-        }
-        .onMoveCommand { moveCommandDirection in
-            switch moveCommandDirection {
-            case .down:
-                text = "Tap down"
-            case .up:
-                text = "Tap up"
-            case .left:
-                text = "Tap left"
-            case .right:
-                text = "Tap right"
-            @unknown default:
-                assertionFailure("Unknown Command Direction")
             }
         }
     }
