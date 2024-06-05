0e8aa4518562e5a47de6d81d04d99e78d57d5926
mkotyak <kotyak.maria@gmail.com>
2024-06-05 13:51:39 +0300

    Implemented tracking of the remote controller swipe gestures

--- TVOSRemoteControlEvents/ContentView.swift
+++ TVOSRemoteControlEvents/ContentView.swift
@@ -1,82 +1,65 @@
-import SwiftUI
 import GameController
+import SwiftUI
 
 struct ContentView: View {
     @State private var text: String = "Hello, world!"
-    
-    init() {
-        debugPrint(GCController.controllers())
+
+    var body: some View {
+        Group {
+            Color.yellow.ignoresSafeArea().overlay {
+                ZStack {
+                    Text(text)
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundStyle(.black)
+
+                    VStack {
+                        Spacer()
+
+                        /// The remote control button must be tapped at least once to add the remote to the GCController.controllers(), otherwise, the array will be empty.
+                        Button("Enable Controller") {
+                            setUpDirectionalPad()
+                        }
+                    }
+                }
+            }
+        }
+        .onExitCommand {
+            text = "Exit"
+        }
+        /// It is used for tracking arrows tap on the remote control
+        .onMoveCommand { moveCommandDirection in
+            switch moveCommandDirection {
+            case .down:
+                text = "Tap down"
+            case .up:
+                text = "Tap up"
+            case .left:
+                text = "Tap left"
+            case .right:
+                text = "Tap right"
+            @unknown default:
+                assertionFailure("Unknown Command Direction")
+            }
+        }
     }
-    
-    
-    func setUpDirectionalPad() {
+
+    /// It is used for tracking swipe on the remote control
+    private func setUpDirectionalPad() {
         debugPrint(GCController.controllers())
         guard let microGamepad = GCController.controllers().first?.microGamepad else {
             debugPrint("No microGamepad")
             return
         }
-        
-        microGamepad.reportsAbsoluteDpadValues = true
-        microGamepad.dpad.valueChangedHandler = { [self] (pad, x, y) in
-            self.text = "\(x):\(y)"
-        }
-    }
 
-    var body: some View {
-        NavigationStack {
-            Group {
-                Color.yellow.ignoresSafeArea()
-                    .overlay {
-                        ZStack {
-                            Text(text)
-                                .font(.largeTitle)
-                                .bold()
-                                .foregroundStyle(.black)
-
-                            VStack {
-                                
-                                Button("Enable Controllers") {
-                                    setUpDirectionalPad()
-                                }
-                                
-                                Spacer()
-
-                                NavigationLink("Tap me") {
-                                    Color.blue.ignoresSafeArea()
-                                        .overlay {
-                                            Text("I am a sheet")
-                                                .font(.largeTitle)
-                                                .bold()
-                                                .foregroundStyle(.white)
-                                                .focusable()
-                                                .onExitCommand {
-                                                    debugPrint("Sheet is exit")
-                                                }
-                                        }
-                                }
-                            }
-                        }
-                    }
-            }
-            .onAppear {
-                setUpDirectionalPad()
-            }
-            .onExitCommand {
-                text = "Exit"
-            }
-            .onMoveCommand { moveCommandDirection in
-                switch moveCommandDirection {
-                case .down:
-                    text = "Tap down"
-                case .up:
-                    text = "Tap up"
-                case .left:
-                    text = "Tap left"
-                case .right:
-                    text = "Tap right"
-                @unknown default:
-                    assertionFailure("Unknown Command Direction")
-                }
+        microGamepad.reportsAbsoluteDpadValues = false
+        microGamepad.dpad.valueChangedHandler = { [self] _, x, _ in
+            if x > 0 {
+                text = "Swiping Right"
+            } else if x < 0 {
+                text = "Swiping Left"
+            } else {
+                text = "Center"
             }
         }
     }
