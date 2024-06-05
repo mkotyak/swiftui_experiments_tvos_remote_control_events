233807546d293b9278473c76e010138b426c3ddc
mkotyak <kotyak.maria@gmail.com>
2024-06-05 11:50:21 +0300

    Implemented .onMoveCommand

--- TVOSRemoteControlEvents/ContentView.swift
+++ TVOSRemoteControlEvents/ContentView.swift
@@ -4,12 +4,32 @@ struct ContentView: View {
     @State private var text: String = "Hello, world!"
 
     var body: some View {
-        Color.yellow.opacity(0.5).ignoresSafeArea()
-            .overlay {
-                Text(text)
-                    .font(.largeTitle)
-                    .bold()
-                    .foregroundStyle(.black)
+        Group {
+            Color.yellow.opacity(0.5).ignoresSafeArea()
+                .overlay {
+                    Text(text)
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundStyle(.black)
+                }
+        }
+        .focusable()
+        .onExitCommand {
+            text = "Exit"
+        }
+        .onMoveCommand { moveCommandDirection in
+            switch moveCommandDirection {
+            case .down:
+                text = "Tap down"
+            case .up:
+                text = "Tap up"
+            case .left:
+                text = "Tap left"
+            case .right:
+                text = "Tap right"
+            @unknown default:
+                assertionFailure("Unknown Command Direction")
             }
+        }
     }
 }
